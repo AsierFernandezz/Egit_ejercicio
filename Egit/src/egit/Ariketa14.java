c6850f834c197ebf19a65f7c51aec97f73c40169
Asier Fernandez <ikcti@plaiaundi.net>
2023-11-16 11:54:47 +0100

    commit inicial

---
+++ Egit/src/egit/Ariketa14.java
@@ -0,0 +1,24 @@
+package egit;
+
+import java.util.Scanner;
+
+public class Ariketa14 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("mete tres numeros");
+		int num1 =Integer.parseInt(entrada.nextLine());
+		int num2 =Integer.parseInt(entrada.nextLine());
+		int num3 =Integer.parseInt(entrada.nextLine());
+		
+		
+		if (num1>num2 && num2>num3) {
+			System.out.println("la cadena no es ascendente");
+		}else {
+			System.out.println("La cadena si es ascendete");
+		}
+	}
+
+}
